0457543a269a4bb82833b97471713f1513de255c
SowmyaNihitha <sowmyanihitha20@gmail.com>
2026-02-02 09:16:02 +0530

    third commit

--- src/patient_encounter_system/database.py
+++ src/patient_encounter_system/database.py
@@ -15,7 +15,7 @@ load_dotenv()
 # -------------------------------------------------
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
-    "sqlite+pysqlite:///:memory:"  # fallback for CI / tests
+    "sqlite+pysqlite:///./test.db"  # fallback for CI / tests
 )
 
 # -------------------------------------------------
